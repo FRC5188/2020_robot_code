6aec535b855a2bdb6827395aa4173a393183d1ea
garrett928 <garrett7621@gmail.com>
2020-01-18 17:08:19 -0500

    adds buttons and axis in constants

--- 2020_robot_code/src/main/java/frc/robot/Constants.java
+++ 2020_robot_code/src/main/java/frc/robot/Constants.java
@@ -9,5 +9,38 @@ public class Constants{
   public static final int rightFalcon2 = 3;
 
   public static final int driverPort = 0;
+  
+	public static class Buttons {
+
+    public static int
+    A = 1,
+    B = 2,
+    X = 3,
+    Y = 4,
+    L = 5,
+    R = 6,
+    BACK = 7,
+    START = 8,
+    L_STICK = 9,
+    R_STICK = 10,
+    TOTAL_BUTTONS = 10;
+}
+  /*
+  Controller axis  (including rTrigger and lTrigger),
+  access with OI.Axis.AXISNAME
+ */
+  public static class Axis {
+
+      public static int
+      LX = 0,
+      LY = 1,
+      LTrigger = 2,
+      RTrigger = 3,
+      RX = 4,
+      RY = 5,
+      AXIS_TOTAL = 6;
+
+  }
+
 
 }
